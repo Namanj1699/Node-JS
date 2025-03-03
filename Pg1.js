a8182ee701d7916c7d7f1acb2a44730bc388eeaf
Naman Jain <jainnaman1699@gmail.com>
2025-03-03 23:26:45 +0530

    first commit

---
+++ Pg1.js
@@ -0,0 +1,12 @@
+console.log("Hello Javascript")
+
+const mul = require('./Pg2');
+
+function sum (a,b)
+{
+    console.log(a+b)
+}
+
+sum(12,5)
+
+mul(5,12)
---
+++ Pg2.js
@@ -0,0 +1,9 @@
+console.log("Hello, Pg2 executing")
+
+function multiply(a,b)
+{
+    let t = a*b;
+    console.log(t);
+}
+
+module.exports = multiply;
